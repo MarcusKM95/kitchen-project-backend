0584d01e55d2d36384aab0d9129fc7001766b770
MarcusKM95 <mamo0005@stud.kea.dk>
2024-11-26 12:38:15 +0100

    Init commit

---
+++ src/main/java/com/example/kitchenservicebackend/KitchenServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchenservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kitchenservicebackend/KitchenServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitchenservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
