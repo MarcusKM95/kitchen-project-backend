f8db51d5cd53ebb4a59427b263fd992a4ea49726
Natia <nate0001@stud.kea.dk>
2024-11-27 16:54:54 +0100

    First commit

---
+++ src/main/java/com/example/kitchenservicebackend/constans/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.example.kitchenservicebackend.constans;
+
+public class SecurityConstants {
+   public static final String JWT_KEY = "mysecurejwtsecretmysecurejwtsecret";
+   public static final String JWT_HEADER = "Authorization";
+}
